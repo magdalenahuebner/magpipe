4b0e2205f2e6a6f1d8fb66c0e27dd24f051662a9
magdalenahuebner <m.huebner@qmul.ac.uk>
2024-03-19 14:12:27 +0000

    added logging info

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,7 +1,12 @@
 # preprocess_data.py
 import sys
 import pandas as pd
-from src.data_wrangling import *
+import logging
+
+from src.data_wrangling import rename_samples_based_on_metadata, filter_canonical_psts, merge_duplicates, log2_transform_and_clean
+
+# Configure logging to display messages of level INFO and higher
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def preprocess_data(aup_data_path, metadata_path, output_path):
     """
@@ -18,27 +23,36 @@ def preprocess_data(aup_data_path, metadata_path, output_path):
     Returns:
     - None: The function saves the preprocessed data to a file specified by output_path.
     """
-    # Load the AUP data and metadata
+    # Load the AUP data
+    logging.info("Loading AUP data.")
     aup = pd.read_csv(aup_data_path, sep='\t', index_col=0)
-    metadata = pd.read_csv(metadata_path, sep='\t', index_col=None)
+
+    # Load metadata for renaming
+    logging.info("Loading metadata for sample renaming.")
+    metadata = pd.read_csv(metadata_path, sep='\t')
 
     # Ensure the column names of AUP data match the first column of metadata for successful preprocessing
     if not set(aup.columns).issubset(set(metadata.iloc[:, 0])):
-        raise ValueError("Column names of AUP data do not match the first column of metadata.")
+        logging.error("Column names of AUP data do not match the first column of metadata.")
 
     # Step 1: Rename columns based on 'sampleID' in metadata if 'sampleID' column is present
+    logging.info("Renaming samples based on metadata.")
     aup = rename_samples_based_on_metadata(aup, metadata)
 
     # Step 2: Filter for canonical phosphorylations
+    logging.info("Filtering for canonical phosphorylations.")
     aup = filter_canonical_psts(aup)
 
-    # Step 3: Remove duplicates
+    # Step 3: Merging duplicate entries
+    logging.info("Merging duplicate entries.")
     aup = merge_duplicates(aup)
 
     # Step 4: Log2 transform and clean data
+    logging.info("Applying log2 transformation and cleaning data.")
     aup = log2_transform_and_clean(aup)
 
     # Save the preprocessed AUP data
+    logging.info(f"Saving preprocessed AUP data to {output_path}.")
     aup.to_csv(output_path, sep='\t')
 
 if __name__ == "__main__":

42badd72033222073ddd58cbcfb2f4b82f2cd9e0
magdalenahuebner <m.huebner@qmul.ac.uk>
2024-03-20 15:26:17 +0000

    added qc: excluding low-quality phosphosites

--- src/data_wrangling.py
+++ src/data_wrangling.py
@@ -139,3 +139,47 @@ def filter_lq_samples(df, metadata, p_quant=0.6, save_path=None):
     logging.info(f"Removed {num_removed_samples} low-quality samples. Retained {len(filtered_df.columns)} samples.")
     
     return filtered_df, filtered_mdat
+
+
+def filter_lq_phosphosites(df, metadata, condition_col, n_rep=2, n_cond=1, p_nas=0.5):
+    """
+    Removes phosphosites with poor quantification across all samples based on specified conditions.
+
+    Parameters:
+    - df (pd.DataFrame): DataFrame containing the phosphoproteomics data with phosphosites as rows and samples as columns.
+    - metadata (pd.DataFrame): DataFrame containing metadata.
+    - condition_col (str): Column name in metadata DataFrame that holds the condition to group by.
+    - n_rep (int): Minimum number of replicates required per phosphosite in at least n_cond conditions.
+    - n_cond (int): Minimum number of conditions where the phosphosite should be present.
+    - p_nas (float): Maximum fraction of NA (missing) values allowed per phosphosite across all samples.
+
+    Returns:
+    - tuple: A tuple containing two elements:
+             1. filtered_data: The DataFrame after removing low-quality phosphosites.
+             2. removed_psts: DataFrame of phosphosites removed due to poor quantification.
+    """
+    # Ensure condition column is present in metadata
+    if condition_col not in metadata.columns:
+        logging.info(f"'metadata' does not contain '{condition_col}' column.")
+
+    # Split data by groups specified in the condition column
+    grouped_data = {grp: df.loc[:, metadata.loc[metadata[condition_col] == grp, 'sampleID']] for grp in metadata[condition_col].unique()}
+
+    # Initialize a DataFrame to store test results
+    test_results = pd.DataFrame(index=df.index, columns=grouped_data.keys())
+
+    # Apply the conditions across each group
+    for grp, grp_data in grouped_data.items():
+        test_results[grp] = grp_data.apply(lambda x: x.count() >= n_rep, axis=1)
+
+    # Determine phosphosites meeting the criteria
+    valid_psts = (test_results.sum(axis=1) >= n_cond) | (df.isnull().mean(axis=1) <= p_nas)
+    
+    # Filter phosphosites based on the conditions
+    filtered_data = df.loc[valid_psts]
+    removed_psts = df.loc[~valid_psts]  # Phosphosites removed due to poor quantification
+
+    # Logging the number of phosphosites removed and retained
+    logging.info(f"Removed {len(removed_psts)} low-quality phosphosites. Retained {len(filtered_data)} phosphosites out of {len(df)} total phosphosites.")
+
+    return filtered_data, removed_psts

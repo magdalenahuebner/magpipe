55a8f8c970b0dc5b55126b9d8a0a2b4d8d10b68e
magdalenahuebner <m.huebner@qmul.ac.uk>
2024-04-07 14:09:36 +0100

    updated package directory in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='magpipe',
-    packages=find_packages(),
+    packages=find_packages(where='src'),  # Finds packages in src
+    package_dir={'': 'src'},  # Tells distutils packages are under src
     version='0.1.0',
     description='A bioinformatics pipeline for performing differential phosphosite occupancy analysis on phosphoproteomics data.',
     author='Magdalena Huebner',

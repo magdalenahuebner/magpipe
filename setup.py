df51128e590b1d8ba3fa3c91abf3128a009ff66e
magdalenahuebner <m.huebner@qmul.ac.uk>
2024-04-07 11:54:58 +0100

    Revert "changed package name"
    This reverts commit 9e29a2cf9ddddf4a75445ea91be69c4cc28ec401.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='magpipe',
+    name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A bioinformatics pipeline for performing differential phosphosite occupancy analysis on phosphoproteomics data.',
+    description='AA bioinformatics pipeline for performing differential phosphosite occupancy analysis on phosphoproteomics data.',
     author='Magdalena Huebner',
     license='MIT',
 )

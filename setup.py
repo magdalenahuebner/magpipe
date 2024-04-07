049b9d862f4a5785a7e4bc482244f2aac8661e12
magdalenahuebner <m.huebner@qmul.ac.uk>
2024-04-07 13:41:42 +0100

    changed package name from src to magpipe

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='magpipe',
     packages=find_packages(),
     version='0.1.0',
     description='A bioinformatics pipeline for performing differential phosphosite occupancy analysis on phosphoproteomics data.',
